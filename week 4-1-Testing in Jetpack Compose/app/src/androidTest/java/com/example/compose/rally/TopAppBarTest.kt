c7adcd8b5bb366df894ff05928efa28678ceea4f
You Been Lee <yblee2001@gmail.com>
2021-12-09 13:22:38 +0900

    TestinCodelab Debugging Test

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,9 +1,8 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsSelectable
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.text.toUpperCase
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
@@ -18,13 +17,11 @@ class TopAppBarTest {
     fun rallyTopAppBarTest() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
-            RallyTheme{
-                RallyTopAppBar(
-                    allScreens = allScreens,
-                    onTabSelected = { }
-                    , currentScreen = RallyScreen.Accounts
-                )
-            }
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { }
+                , currentScreen = RallyScreen.Accounts
+            )
         }
 
         composeTestRule
@@ -32,4 +29,21 @@ class TopAppBarTest {
             .assertIsSelected()
     }
 
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { }
+                , currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
+    }
 }
