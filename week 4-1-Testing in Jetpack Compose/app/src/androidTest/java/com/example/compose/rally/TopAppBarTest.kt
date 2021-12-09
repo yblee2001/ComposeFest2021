f09679bb93d19003561b2b86f8a91da099b4a47f
You Been Lee <yblee2001@gmail.com>
2021-12-09 13:30:09 +0900

    TestingCodealab Semantic Tree

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -19,8 +19,7 @@ class TopAppBarTest {
         composeTestRule.setContent {
             RallyTopAppBar(
                 allScreens = allScreens,
-                onTabSelected = { }
-                , currentScreen = RallyScreen.Accounts
+                onTabSelected = { }, currentScreen = RallyScreen.Accounts
             )
         }
 
@@ -35,15 +34,18 @@ class TopAppBarTest {
         composeTestRule.setContent {
             RallyTopAppBar(
                 allScreens = allScreens,
-                onTabSelected = { }
-                , currentScreen = RallyScreen.Accounts
+                onTabSelected = { }, currentScreen = RallyScreen.Accounts
             )
         }
 
-        composeTestRule.onRoot().printToLog("currentLabelExists")
-
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
